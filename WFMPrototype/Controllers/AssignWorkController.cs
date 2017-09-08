c268997161ce04283d506387e6db7ef808f1d1eb
Raghu <RaghunathSingh.rao@etechtexas.com>
2017-09-09 01:56:06 +0530

    changes

---
+++ WFMPrototype/Controllers/AssignWorkController.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WFMPrototype.DAL;
+using WFMPrototype.Models;
+
+namespace WFMPrototype.Controllers
+{
+    public class AssignWorkController : Controller
+    {
+        // GET: AssignWork
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [CheckSession]
+        public ActionResult AssignWork(tbl_assignwork assignworkdetails)
+        {
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+                    //var checkexist = db.tbl_supervisors.Where(a => a.EmailID.ToLower() == supervisordetails.EmailID.ToLower() && a.IsActive == true && a.OrgID == SessionInfo.OrgID).FirstOrDefault();
+                    //if (checkexist != null)
+                    //{
+                    //    return Json("2", JsonRequestBehavior.AllowGet);
+                    //}
+                    tbl_assignwork assignworkentity = new tbl_assignwork();
+                    assignworkentity.CompanyName = assignworkdetails.CompanyName;
+                    assignworkentity.WorkerName = assignworkdetails.WorkerName;
+                    assignworkentity.ShiftName = assignworkdetails.ShiftName;
+                    assignworkentity.Checklist = assignworkdetails.Checklist;
+                    assignworkentity.WorkInterVal = assignworkdetails.WorkInterVal;
+                    assignworkentity.IsActive = true;
+                    assignworkentity.OrgID = SessionInfo.OrgID;
+                    assignworkentity.ModifyBy = SessionInfo.Username;
+                    assignworkentity.ModifyDate = System.DateTime.Today.AddHours(5).ToShortDateString();
+                    db.tbl_assignworks.InsertOnSubmit(assignworkentity);
+                    db.SubmitChanges();
+                    return Json("1", JsonRequestBehavior.AllowGet);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Json("0", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [CheckSession]
+        public ActionResult AssignedWorkList()
+        {
+            dynamic AssignedWorkList = null;
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+
+                    AssignedWorkList = db.tbl_assignworks.Where(a => a.IsActive == true && a.OrgID == SessionInfo.OrgID).ToList();
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+            return Json(AssignedWorkList, JsonRequestBehavior.AllowGet);
+        }
+
+        [CheckSession]
+        public ActionResult GetDataByAssignWorkID(int AssignWorkID)
+        {
+            dynamic AssignedWorkData = null;
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+
+                    AssignedWorkData = db.tbl_assignworks.Where(a => a.AssignWorkID == AssignWorkID && a.OrgID == SessionInfo.OrgID).FirstOrDefault();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Json(AssignedWorkData, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        [CheckSession]
+        public void UpdateAssignedWork(tbl_assignwork assignworkdetails)
+        {
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+                    tbl_assignwork supervisorentity = db.tbl_assignworks.Single(a => a.AssignWorkID == assignworkdetails.AssignWorkID && a.OrgID == SessionInfo.OrgID);
+                    tbl_assignwork assignworkentity = new tbl_assignwork();
+                    assignworkentity.CompanyName = assignworkdetails.CompanyName;
+                    assignworkentity.WorkerName = assignworkdetails.WorkerName;
+                    assignworkentity.ShiftName = assignworkdetails.ShiftName;
+                    assignworkentity.Checklist = assignworkdetails.Checklist;
+                    assignworkentity.WorkInterVal = assignworkdetails.WorkInterVal;
+                    assignworkentity.IsActive = true;
+                    assignworkentity.OrgID = SessionInfo.OrgID;
+                    assignworkentity.ModifyBy = SessionInfo.Username;
+                    assignworkentity.ModifyDate = System.DateTime.Today.AddHours(5).ToShortDateString();
+                    db.SubmitChanges();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        [CheckSession]
+        public ActionResult RemoveAssignedWork(int AssignWorkID)
+        {
+
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+                    tbl_assignwork Obj = new tbl_assignwork();
+                    Obj = db.tbl_assignworks.Where(a => a.AssignWorkID == AssignWorkID && a.OrgID == SessionInfo.OrgID).FirstOrDefault();
+                    Obj.IsActive = false;
+                    db.SubmitChanges();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Json("0", JsonRequestBehavior.AllowGet);
+        }
+        [CheckSession]
+        public ActionResult CompanyList()
+        {
+            List<tbl_company> CompanyList = new List<tbl_company>();
+
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+                    var Companies = from a in new WFMLiveDataContext().tbl_companies
+                                  where a.IsActive == true && a.OrgID == SessionInfo.OrgID
+                                  select new { a.CompanyName };
+                    CompanyList = Companies.AsEnumerable()
+                          .Select(o => new tbl_company
+                          {
+                              CompanyName = o.CompanyName,
+                          }).ToList();
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+            return Json(CompanyList, JsonRequestBehavior.AllowGet);
+        }
+        [CheckSession]
+        public ActionResult WorkerList()
+        {
+            List<tbl_worker> WorkerList = new List<tbl_worker>();
+
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+                    var Workers = from a in new WFMLiveDataContext().tbl_workers
+                                  where a.IsActive == true && a.OrgID == SessionInfo.OrgID
+                                  select new { a.FirstName };
+                    WorkerList = Workers.AsEnumerable()
+                          .Select(o => new tbl_worker
+                          {
+                              FirstName= o.FirstName,
+                          }).ToList();
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+            return Json(WorkerList, JsonRequestBehavior.AllowGet);
+        }
+        [CheckSession]
+        public ActionResult ShiftList()
+        {
+            List<tbl_shift> ShiftList = new List<tbl_shift>();
+            
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+                    var Shifts = from a in new WFMLiveDataContext().tbl_shifts
+                                  where a.IsActive == true && a.OrgID == SessionInfo.OrgID
+                                  select new { a.ShiftName };
+                    ShiftList = Shifts.AsEnumerable()
+                          .Select(o => new tbl_shift
+                          {
+                              ShiftName = o.ShiftName,
+                          }).ToList();
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+            return Json(ShiftList, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ WFMPrototype/JS/AssignWork/AssignWork.js
@@ -0,0 +1,296 @@
+﻿$.validator.setDefaults({
+    submitHandler: function () {
+        if ($("#hdnassignework").val() == "1") {
+            UpdateAssignwork();
+        }
+        else {
+            var assignworkdetails = {};
+            assignworkdetails.CompanyName = $("#ddlcompanyname").val();
+            assignworkdetails.WorkerName = $("#ddlworkername").val();
+            assignworkdetails.ShiftName = $("#ddlshift").val();
+          //  assignworkdetails.Checklist = $("#chkchecklist").val();
+            assignworkdetails.WorkInterVal = $("#txtworkinterval").val();
+            
+            $.ajax({
+                type: "POST",
+                url: "AssignWork/AssignWork",
+                data: assignworkdetails,
+                cache: false,
+                success: function (data) {
+
+                    if (data == 1) {
+                        $.alert({
+                            title: '',
+                            content: 'Work assigned successfully!',
+                            type: 'green',
+                        });
+                        LoadSupervisors();
+                    }
+                    //else if (data == 2) {
+                    //    $.alert({
+                    //        title: '',
+                    //        content: 'Supervisor email id already exist!',
+                    //        type: 'red',
+                    //    });
+                    //}
+                    else {
+                        $.alert({
+                            title: '',
+                            content: 'Something went wrong. please try after sometime!',
+                            type: 'red',
+                        });
+                    }
+                }
+            });
+        }
+    }
+});
+
+
+
+$(document).ready(function () {
+
+    $("#assignworkform").validate({
+        rules: {
+            ddlcompanyname: "required",
+            ddlworkername: "required",
+            ddlshift: "required",
+            chkchecklist: "required",            
+            txtworkinterval: "required",
+        },
+        messages: {
+            ddlcompanyname: "Please select company name",
+            ddlworkername: "Please select worker name",
+            ddlshift: "Please select shift name",
+            chkchecklist: "Please enter father name",           
+            txtworkinterval: "Please enter work interval",
+
+        }
+    });
+
+    //end
+    // page validaton using jquery
+    $("#spantext").text("Assign Work");
+    // load Organization
+    BindCompany();
+    BindWorker();
+    BindShift();
+    LoadAssignedWork();
+});
+
+function Cancel() {
+
+    $("#spantext").text("Assign Work");
+    $('#assignworkform')[0].reset();
+    $("#hdnassignwork").val(0);
+    $("#hdnassignworkID").val(0);
+}
+function LoadAssignedWork() {
+    $.ajax({
+        type: "GET",
+        url: "AssignWork/AssignedWorkList",
+        data: "",
+        cache: false,
+        success: function (data) {
+
+            $('#tblAssignedWork').DataTable().destroy();
+            $("#tblAssignedWork tbody").empty();
+
+            var tabledatabody = '';
+            $.each(data, function (index, value) {
+                tabledatabody += '<tr>';
+
+                tabledatabody += ' <td>' + value.CompanyName + '</td>';
+                tabledatabody += ' <td>' + value.WorkerName + '</td>';
+                tabledatabody += ' <td>' + value.ShiftName + '</td>';
+                tabledatabody += ' <td>' + value.Checklist + '</td>';
+                tabledatabody += ' <td>' + value.WorkInterVal + '</td>';               
+                tabledatabody += ' <td><a href="javascript:void(0);" onclick=EditAssignedWork(' + value.AssignWorkID + ')> <i class="glyphicon glyphicon-edit"></i></a></td>';
+                tabledatabody += ' <td><a href="javascript:void(0);" onclick=RemoveAssignedWork(' + value.AssignWorkID + ')><i class="glyphicon glyphicon-remove-sign"></i></a></td>';
+                tabledatabody += ' </tr>';
+            });
+            $("#tblAssignedWork tbody").append(tabledatabody);
+            $.fn.dataTable.ext.errMode = 'none';
+            $('#tblAssignedWork').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": true,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true
+            });
+        }
+    });
+}
+
+
+function EditAssignedWork(AssignWorkID) {
+    $("#hdnassignwork").val(1);
+    $("#hdnassignworkID").val(SupervisorID)
+    $('#AssignWork').trigger('click');
+    $("#spantext").text("Edit Assigned Work");
+    $.ajax({
+        type: "GET",
+        url: "AssignWork/GetDataByAssignWorkID",
+        data: { AssignWorkID: AssignWorkID },
+        cache: false,
+        success: function (data) {
+            $("#ddlcompanyname").val(data.CompanyName);
+            $("#ddlworkername").val(data.WorkerName);
+            $("#ddlshift").val(data.ShiftName);
+            $("#chkchecklist").val(data.Checklist);
+            $("#txtinterval").val(data.WorkInterVal);           
+        }
+    });
+}
+function UpdateAssignedWork() {
+    var assignworkdetails = {};
+
+    assignworkdetails.CompanyName = $("#ddlcompanyname").val();
+    assignworkdetails.WorkerName = $("#ddlworkername").val();
+    assignworkdetails.ShiftName = $("#ddlshift").val();
+    //  assignworkdetails.Checklist = $("#chkchecklist").val();
+    assignworkdetails.WorkInterVal = $("#txtworkinterval").val();
+    assignworkdetails.AssignWorkID = $("#hdnassignworkID").val();
+    $.ajax({
+        type: "POST",
+        url: "AssignWork/UpdateAssignedWork",
+        data: assignworkdetails,
+        cache: false,
+        success: function (data) {
+
+            $("#hdnassignworkID").val(0);
+            $("#hdnassignwork").val(0);
+            $("#spantext").text("Assign Work");
+            LoadAssignedWork();
+            $("#ddlcompanyname").val("");
+            $("#ddlworkername").val("");
+            $("#ddlshift").val("");
+            $("#chkchecklist").val("");
+            $("#txtinterval").val("");          
+            $.alert({
+                title: '',
+                content: 'Work assignment updated successfully!',
+                type: 'green',
+            });
+            LoadAssignedWork();
+
+        },
+        error: function (error) {
+            $.alert({
+                title: '',
+                content: 'Something went wrong. please try after sometime!',
+                type: 'red',
+
+            });
+        }
+
+    });
+
+}
+
+function RemoveAssignedWork(AssignWorkID) {
+
+
+    $.ajax({
+        type: "GET",
+        url: "AssignWork/RemoveAssignedWork",
+        data: { AssignWorkID: AssignWorkID },
+        cache: false,
+        success: function (data) {
+
+            LoadAssignedWork();
+            $.alert({
+                title: '',
+                content: 'Work assignment deleted successfully!',
+                type: 'green',
+            });
+        }
+    });
+}
+
+function BindCompany() {
+   
+    $.ajax({
+        type: "GET",
+        url: "AssignWork/CompanyList",
+        data: "",
+        cache: false,
+        success: function (data) {         
+            $("#ddlcompanyname").html("");
+            $('#ddlcompanyname').append($('<option>', {
+                value: "",
+                text:"Select company"
+            }));
+            $.each(data, function (i, value) {
+                $('#ddlcompanyname').append($('<option>', {
+                    value: value.CompanyName,
+                    text: value.CompanyName
+                }));
+            })
+        },
+        error: function (error) {
+            $.alert({
+                title: '',
+                content: 'Something went wrong. please try after sometime!',
+                type: 'red',
+
+            });
+        },
+    });
+}
+function BindWorker() {
+    $.ajax({
+        type: "GET",
+        url: "AssignWork/WorkerList",
+        data: "",
+        cache: false,
+        success: function (data) {
+            $("#ddlworkername").html("");
+            $('#ddlworkername').append($('<option>', {
+                value: "",
+                text: "Select worker"
+            }));
+            $.each(data, function (i, value) {
+                $("#ddlworkername").append($('<option></option>').val(value.WorkerID).html(value.FirstName));
+            })
+        },
+        error: function (error) {
+            $.alert({
+                title: '',
+                content: 'Something went wrong. please try after sometime!',
+                type: 'red',
+
+            });
+        },
+    });
+}
+
+function BindShift() {
+    $.ajax({
+        type: "GET",
+        url: "AssignWork/ShiftList",
+        data: "",
+        cache: false,
+        success: function (data) {
+            $("#ddlshift").html("");
+            $('#ddlshift').append($('<option>', {
+                value: "",
+                text: "Select shift"
+            }));
+            $.each(data, function (i, value) {
+                $("#ddlshift").append($('<option></option>').val(value.ShiftID).html(value.ShiftName))
+              
+            })
+        },
+        error: function (error) {
+            $.alert({
+                title: '',
+                content: 'Something went wrong. please try after sometime!',
+                type: 'red',
+
+            });
+        },
+    });
+}
