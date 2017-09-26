4010001004b450db685189ea0c64555714a074a1
suraj <surajkumarghosi@gmail.com>
2017-09-27 01:50:54 +0530

    last

--- WFMPrototype/Controllers/ChecklistController.cs
+++ WFMPrototype/Controllers/ChecklistController.cs
@@ -30,20 +30,27 @@ namespace WFMPrototype.Controllers
 
         public JsonResult Addchecklist(CheckList checklistdetails)
         {
-            dynamic CompanyList=null;
+           
             try
             {
                 using (var db = new WFMLiveDataContext())
                 {
-                    
-                    foreach(var shift in checklistdetails.Shiftlist)
+                    var chekcworknameexits = db.tbl_checklists.Where(a => a.OrgID == SessionInfo.OrgID && a.WorkName.Trim().ToLower() == checklistdetails.WorkName.ToLower() && a.CompanyID == checklistdetails.CompanyID).FirstOrDefault();
+                    if (chekcworknameexits != null)
                     {
+                        return Json("2", JsonRequestBehavior.AllowGet);
+                    }
+
+                    foreach (var shift in checklistdetails.Shiftlist)
+                    {
+                        
                         tbl_checklist checkentity = new tbl_checklist();
                         checkentity.OrgID = SessionInfo.OrgID;
                         checkentity.WorkName = checklistdetails.WorkName;
                         checkentity.WorkInterval = checklistdetails.WorkInterval;
                         checkentity.CompanyID = checklistdetails.CompanyID;
                         checkentity.ShiftID = Convert.ToInt32(shift);
+                        checkentity.IsActive = true;
                         db.tbl_checklists.InsertOnSubmit(checkentity);
                         db.SubmitChanges();
                     }
@@ -55,8 +62,47 @@ namespace WFMPrototype.Controllers
 
             }
 
-            return Json(CompanyList, JsonRequestBehavior.AllowGet);
+            return Json("1", JsonRequestBehavior.AllowGet);
 
+        }
+
+
+        public JsonResult Createchecklist()
+        {
+            dynamic checklist = null;
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+                    checklist = (from tempchcklist in db.tbl_checklists
+                                 join temcom in db.tbl_companies on tempchcklist.CompanyID equals temcom.CompanyID
+                                 join tempshift in db.tbl_shifts on tempchcklist.ShiftID equals tempshift.ShiftID where tempchcklist.OrgID==SessionInfo.OrgID && tempchcklist.IsActive==true
+                                 select new { checklistid=tempchcklist.ChecklistID,companyid=temcom.CompanyID,shiftid=tempshift.ShiftID,shiftstarttime=tempshift.StartTime,shiftendtime=tempshift.EndTime, companyname=temcom.CompanyName,workname=tempchcklist.WorkName,workinterval=tempchcklist.WorkInterval,shiftname=tempshift.ShiftName }).ToList().OrderBy(a=>a.workname);
+                                 //db.tbl_checklists.Where(a => a.OrgID == SessionInfo.OrgID && a.IsActive == true).ToList();
+                }
+            }catch(Exception ex)
+            {
+
+            }
+            return Json(checklist, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult GetWorkdetailbyname(string workname,int companyid)
+        {
+            dynamic workdeails = string.Empty;
+            try
+            {
+                using (var db = new WFMLiveDataContext())
+                {
+                    workdeails = db.tbl_checklists.Where(a => a.CompanyID == companyid && a.OrgID == SessionInfo.OrgID && a.WorkName == workname).ToList();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return Json(workdeails, JsonRequestBehavior.AllowGet);
         }
     }
 }
